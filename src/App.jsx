a59be840b4b6b8dfdc00b0142bcc74404b513257
Jonathan Mazin <jmazin@brainstation.io>
2025-03-04 15:18:10 -0500

    fix header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <header>This is our header</header>
+      <header>This is our header. Isn't it nice</header>
       <h1>InStock</h1>
     </>
   );
