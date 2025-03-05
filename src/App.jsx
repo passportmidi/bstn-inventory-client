f32d561924910a2c443acf3d626707b828ca694d
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-05 13:02:22 -0400

    move warehouses list into Warehouses page, remove extra .gitkeep file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.scss";
-import WarehouseList from "./components/WarehouseList/WarehouseList";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Warehouses from "./pages/Warehouses/Warehouses";
@@ -32,7 +31,6 @@ const App = () => {
           <Route path="/" element={<Warehouses />} />
         </Routes>
       </div>
-      <WarehouseList />
     </BrowserRouter>
   );
 };
--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -1,3 +1,10 @@
+import WarehouseList from "../../components/WarehouseList/WarehouseList.jsx";
+
 export default function Warehouses() {
-    return <h1>Warehouses</h1>
+  return (
+    <>
+      <h1>Warehouses</h1>
+      <WarehouseList />
+    </>
+  );
 }
