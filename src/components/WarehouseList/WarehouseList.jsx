a013c8515b899ad164c417065a5c53184818fbf5
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-04 22:38:37 -0400

    add class names and minor styling

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,28 +1,35 @@
 import "./WarehouseList.scss";
+import deleteLogo from "../../assets/icons/delete_outline-24px.svg";
+import editLogo from "../../assets/icons/edit-24px.svg";
 
 function WarehouseList() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Warehouse</th>
-          <th>Address</th>
-          <th>Contact Name</th>
-          <th>Contact Information</th>
-          <th>Actions</th>
+    <table className="table">
+      <thead className="table__head">
+        <tr className="table__row">
+          <th className="table__cell table__cell--heading">Warehouse</th>
+          <th className="table__cell table__cell--heading">Address</th>
+          <th className="table__cell table__cell--heading">Contact Name</th>
+          <th className="table__cell table__cell--heading">
+            Contact Information
+          </th>
+          <th className="table__cell table__cell--heading">Actions</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td>Manhattan</td>
-          <td>503 Broadway, New York, USA</td>
-          <td>Parmin Aujla</td>
-          <td>
+      <tbody className="table__body">
+        <tr className="table__row">
+          <td className="table__cell">Manhattan</td>
+          <td className="table__cell">503 Broadway, New York, USA</td>
+          <td className="table__cell">Parmin Aujla</td>
+          <td className="table__cell">
             +1 (629) 555-0129
             <br />
             paujla@instock.com
           </td>
-          <td>Icons</td>
+          <td className="table__cell">
+            <img src={deleteLogo} alt="Delete" />
+            <img src={editLogo} alt="Edit" />
+          </td>
         </tr>
       </tbody>
     </table>
