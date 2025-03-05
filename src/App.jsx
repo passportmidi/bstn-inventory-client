c8260a29672342978b78e8d8c795c664ceee1446
Brinda Patel <brindap37@gmail.com>
2025-03-05 10:16:05 -0500

    warehouse details updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 
 function App() {
   return (
     <>
       <h1>InStock</h1>
+      <WarehouseDetails />
     </>
   );
 }
--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 export const baseURL = import.meta.env.VITE_API_URL;
-import backArrow from "../../assets/icons/arrow_back-24px";
-import editButton from "../../assets/icons/edit-white-24px";
+import backArrow from "../../assets/icons/arrow_back-24px.svg";
+import editButton from "../../assets/icons/edit-white-24px.svg";
 import "./WarehouseDetails.scss";
 
 export default function WarehouseDetails() {
