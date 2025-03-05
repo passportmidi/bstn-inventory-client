c4e9a3d2dc072401e1f67f81cf04b47f9ff90005
Brinda Patel <brindap37@gmail.com>
2025-03-04 23:04:06 -0500

    warehouse details changes plus .env file

---
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+export const baseURL = import.meta.env.VITE_API_URL;
+import backArrow from "../../assets/icons/arrow_back-24px";
+import editButton from "../../assets/icons/edit-white-24px";
+import "./WarehouseDetails.scss";
+
+export default function WarehouseDetails() {
+  const { id } = useParams();
+  const [warehouse, setWarehouse] = useState(null);
+
+  useEffect(() => {
+    fetchWarehouse();
+  }, [id]);
+
+  async function fetchWarehouse() {
+    try {
+      const { data } = await axios.get(`${baseURL}/api/warehouse/${id}`);
+      setWarehouse(data);
+    } catch (e) {
+      console.log("Error fetching warehouse:", e);
+    }
+  }
+  if (!warehouse) {
+    return <div>Loading Warehouse...</div>;
+  }
+  return (
+    <>
+      <h1> HI </h1>
+    </>
+  );
+}
