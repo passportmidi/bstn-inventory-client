5f855ffbf029627c9ae21375d15254e4ab1e293e
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-06 15:20:41 -0400

    arrange items in responsive grid

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -32,30 +32,38 @@ export default function Warehouses() {
       {warehouses.map((warehouse) => {
         return (
           <div className="warehouse__row" key={warehouse.id}>
-            <div class="warehouse__cell">
-              <div className="warehouse__cell-label">Warehouse</div>
-              <div className="warehouse__name">{warehouse.warehouse_name}</div>
-            </div>
-            <div className="warehouse__cell">
-              <div className="warehouse__cell-label">Address</div>
-              <div className="warehouse__address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</div>
-            </div>
-            <div className="warehouse__cell">
-              <div className="warehouse__cell-label">Contact Name</div>
-              <div className="warehouse__contact">{warehouse.contact_name}</div>
-            </div>
-            <div className="warehouse__cell">
-              <div className="warehouse__cell-label">Contact Information</div>
-              <div className="warehouse__contact-info">
-                <div className="warehouse__phone">
-                  {warehouse.contact_phone}
+            <div class="warehouse__column">
+              <div class="warehouse__cell">
+                <div className="warehouse__cell-label">Warehouse</div>
+                <div className="warehouse__name">
+                  {warehouse.warehouse_name}
                 </div>
-                <div className="warehouse__email">
-                  {warehouse.contact_email}
+              </div>
+              <div className="warehouse__cell">
+                <div className="warehouse__cell-label">Address</div>
+                <div className="warehouse__address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</div>
+              </div>
+            </div>
+            <div class="warehouse__column">
+              <div className="warehouse__cell">
+                <div className="warehouse__cell-label">Contact Name</div>
+                <div className="warehouse__contact">
+                  {warehouse.contact_name}
+                </div>
+              </div>
+              <div className="warehouse__cell">
+                <div className="warehouse__cell-label">Contact Information</div>
+                <div className="warehouse__contact-info">
+                  <div className="warehouse__phone">
+                    {warehouse.contact_phone}
+                  </div>
+                  <div className="warehouse__email">
+                    {warehouse.contact_email}
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="warehouse__cell">
+            <div className="warehouse__cell warehouse__cell--actions">
               <div className="warehouse__cell-label">Actions</div>
               <div className="warehouse__actions">
                 <img src={deleteLogo} alt="Delete" />
