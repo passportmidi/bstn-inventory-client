aa96cecc9c80a49869ccb98d83a9d0e52a0e8dcc
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-06 13:23:26 -0400

    add cell labels and hide them on tablet and desktop views

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -32,16 +32,35 @@ export default function Warehouses() {
       {warehouses.map((warehouse) => {
         return (
           <div className="warehouse__row" key={warehouse.id}>
-            <div className="warehouse__name">{warehouse.warehouse_name}</div>
-            <div className="warehouse__address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</div>
-            <div className="warehouse__contact">{warehouse.contact_name}</div>
-            <div className="warehouse__contact-info">
-              <div className="warehouse__phone">{warehouse.contact_phone}</div>
-              <div className="warehouse__email">{warehouse.contact_email}</div>
+            <div class="warehouse__cell">
+              <div className="warehouse__cell-label">Warehouse</div>
+              <div className="warehouse__name">{warehouse.warehouse_name}</div>
             </div>
-            <div className="warehouse__actions">
-              <img src={deleteLogo} alt="Delete" />
-              <img src={editLogo} alt="Edit" />
+            <div className="warehouse__cell">
+              <div className="warehouse__cell-label">Address</div>
+              <div className="warehouse__address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</div>
+            </div>
+            <div className="warehouse__cell">
+              <div className="warehouse__cell-label">Contact Name</div>
+              <div className="warehouse__contact">{warehouse.contact_name}</div>
+            </div>
+            <div className="warehouse__cell">
+              <div className="warehouse__cell-label">Contact Information</div>
+              <div className="warehouse__contact-info">
+                <div className="warehouse__phone">
+                  {warehouse.contact_phone}
+                </div>
+                <div className="warehouse__email">
+                  {warehouse.contact_email}
+                </div>
+              </div>
+            </div>
+            <div className="warehouse__cell">
+              <div className="warehouse__cell-label">Actions</div>
+              <div className="warehouse__actions">
+                <img src={deleteLogo} alt="Delete" />
+                <img src={editLogo} alt="Edit" />
+              </div>
             </div>
           </div>
         );
