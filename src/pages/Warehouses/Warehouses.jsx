13f99f113a3d9ab59ec109e4c455129dadf76f67
Jonathan Mazin <jmazin@brainstation.io>
2025-03-06 11:09:23 -0500

    play around with grid

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -29,25 +29,23 @@ export default function Warehouses() {
   return (
     <>
       <h1>Warehouses</h1>
-      <div className="warehouse__grid">
-        {warehouses.map((warehouse) => {
-          return (
-            <div className="warehouse__row" key={warehouse.id}>
-              <div className="warehouse__name">{warehouse.warehouse_name}</div>
-              <div className="warehouse__address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</div>
-              <div className="warehouse__contact">{warehouse.contact_name}</div>
-              <div className="warehouse__contact-info">
-                <div className="warehouse__phone">{warehouse.contact_phone}</div>
-                <div className="warehouse__email">{warehouse.contact_email}</div>
-              </div>
-              <div className="warehouse__actions">
-                <img src={deleteLogo} alt="Delete" />
-                <img src={editLogo} alt="Edit" />
-              </div>
+      {warehouses.map((warehouse) => {
+        return (
+          <div className="warehouse__row" key={warehouse.id}>
+            <div className="warehouse__name">{warehouse.warehouse_name}</div>
+            <div className="warehouse__address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</div>
+            <div className="warehouse__contact">{warehouse.contact_name}</div>
+            <div className="warehouse__contact-info">
+              <div className="warehouse__phone">{warehouse.contact_phone}</div>
+              <div className="warehouse__email">{warehouse.contact_email}</div>
             </div>
-          );
-        })}
-      </div>
+            <div className="warehouse__actions">
+              <img src={deleteLogo} alt="Delete" />
+              <img src={editLogo} alt="Edit" />
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 }
