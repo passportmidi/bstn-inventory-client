45ff49c6865e0834f02aa9cd93b50da8dcdb3582
Jacob Fattahi <jacobfattahi@gmail.com>
2025-03-06 16:55:38 -0400

    add footer

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 
 
@@ -5,6 +6,7 @@ export default function Warehouses() {
     return (
         <>
         <Header />
+        <Footer />
         </>
     )
 }
