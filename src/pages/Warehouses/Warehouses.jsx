c05ecd0550c167e7fcd10c6693e76f57bfa26cbc
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-07 12:33:46 -0400

    add styling around page and padding to header

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 import { useEffect, useState } from "react";
 import deleteLogo from "../../assets/icons/delete_outline-24px.svg";
@@ -31,57 +30,58 @@ export default function Warehouses() {
 
   return (
     <>
-      <Header />
-      <h1>Warehouses</h1>
-      {warehouses.map((warehouse) => {
-        return (
-          <div className="warehouse__row" key={warehouse.id}>
-            <div class="warehouse__column">
-              <div class="warehouse__cell">
-                <div className="warehouse__cell-label">Warehouse</div>
-                <div className="warehouse__cell-item warehouse__name">
-                  {warehouse.warehouse_name}
-                </div>
-              </div>
-              <div className="warehouse__cell">
-                <div className="warehouse__cell-label">Address</div>
-                <div className="warehouse__cell-item warehouse__address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</div>
-              </div>
-            </div>
-            <div class="warehouse__column">
-              <div className="warehouse__cell">
-                <div className="warehouse__cell-label">Contact Name</div>
-                <div className="warehouse__cell-item warehouse__contact">
-                  {warehouse.contact_name}
-                </div>
-              </div>
-              <div className="warehouse__cell">
-                <div className="warehouse__cell-label">Contact Information</div>
-                <div className="warehouse__contact-info">
-                  <div className="warehouse__cell-item warehouse__phone">
-                    {warehouse.contact_phone}
-                  </div>
-                  <div className="warehouse__cell-item warehouse__email">
-                    {warehouse.contact_email}
+      <div className="warehouse">
+        <div className="warehouse__head">
+          <h1 className="warehouse__header">Warehouses</h1>
+        </div>
+        {warehouses.map((warehouse) => {
+          return (
+            <div className="warehouse__row" key={warehouse.id}>
+              <div class="warehouse__column">
+                <div class="warehouse__cell">
+                  <div className="warehouse__cell-label">Warehouse</div>
+                  <div className="warehouse__cell-item warehouse__name">
+                    {warehouse.warehouse_name}
                   </div>
                 </div>
+                <div className="warehouse__cell">
+                  <div className="warehouse__cell-label">Address</div>
+                  <div className="warehouse__cell-item warehouse__address">{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</div>
+                </div>
+              </div>
+              <div class="warehouse__column">
+                <div className="warehouse__cell">
+                  <div className="warehouse__cell-label">Contact Name</div>
+                  <div className="warehouse__cell-item warehouse__contact">
+                    {warehouse.contact_name}
+                  </div>
+                </div>
+                <div className="warehouse__cell">
+                  <div className="warehouse__cell-label">
+                    Contact Information
+                  </div>
+                  <div className="warehouse__contact-info">
+                    <div className="warehouse__cell-item warehouse__phone">
+                      {warehouse.contact_phone}
+                    </div>
+                    <div className="warehouse__cell-item warehouse__email">
+                      {warehouse.contact_email}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="warehouse__cell warehouse__cell--actions">
+                <div className="warehouse__cell-label">Actions</div>
+                <div className="warehouse__cell-item warehouse__actions">
+                  <img src={deleteLogo} alt="Delete" />
+                  <img src={editLogo} alt="Edit" />
+                </div>
               </div>
             </div>
-            <div className="warehouse__cell warehouse__cell--actions">
-              <div className="warehouse__cell-label">Actions</div>
-              <div className="warehouse__cell-item warehouse__actions">
-                <img src={deleteLogo} alt="Delete" />
-                <img src={editLogo} alt="Edit" />
-              </div>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <Footer />
     </>
   );
-=======
-export default function Warehouses() {
-  return <h1>Warehouses</h1>;
->>>>>>> develop
 }
