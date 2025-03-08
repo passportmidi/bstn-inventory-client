e31bf57144248204940c22f76ca29f14c274bb53
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-03-07 21:50:51 -0400

    add search bar to warehouses page

--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -6,6 +6,7 @@ import chevronLogo from "../../assets/icons/chevron_right-24px.svg";
 import sortLogo from "../../assets/icons/sort-24px.svg";
 import "./Warehouses.scss";
 import { Link } from "react-router-dom";
+import searchButton from "../../assets/icons/search-24px.svg";
 
 const baseURL = import.meta.env.VITE_API_URL;
 
@@ -32,8 +33,23 @@ export default function Warehouses() {
   return (
     <>
       <div className="warehouse">
-        <div className="warehouse__head">
-          <h1 className="warehouse__header">Warehouses</h1>
+        <div className="head">
+          <div className="head__header">
+            <h1 className="head__header--name">Warehouses</h1>
+          </div>
+          <div className="head__actions">
+            <button className="head__search">
+              <span className="head__search--desc"> Search... </span>
+              <img
+                className="head__search--icon"
+                src={searchButton}
+                alt="search-icon"
+              />
+            </button>
+            <Link to={`/warehouse/add`} className="head__add-link">
+              <div className="head__add"> + Add New Warehouse </div>
+            </Link>
+          </div>
         </div>
         {warehouses.map((warehouse) => {
           return (
